18a8fbf87652479281d9f302a8e5ed0c8ce4971e
jacob.yao <jacob@blackcape.io>
2024-01-22 13:17:49 -0500

    Revert unrelated changes
    This reverts commit e2c759aa0f5d66eb163ec590e286280f2cef6e9a.

--- tprc/streamer.py
+++ tprc/streamer.py
@@ -1,7 +1,5 @@
 import socket
 import pickle
-import RPi.GPIO as GPIO
-import time
 from typing import Optional
 
 
@@ -9,36 +7,6 @@ class Streamer:
     """Streamer class for sending data from device to LibEMG pipeline"""
     def __init__(self) -> None:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        self.channelNum = None
-        self.pins = []
-
-    def setup_board(self):
-        GPIO.setmode(GPIO.BOARD) #number closen for pins correlates directly w number on board
-        self.channelNum = input("Select number of desired channels:")
-
-        pinNum_long = int(input("Select desired pin numbers (seperate w spaces):"))
-        self.pins = [int(pin) for pin in pinNum_long.split()]
-
-
-        for i in range(len(self.channelNum)):
-          GPIO.setup(self.pins[i], GPIO.IN)
-
-    def read_pins(self):
-        adcSamp = 0 #to be edited when found
-        running = True #forever while loop for streaming
-        wait = 1/adcSamp
-
-        #inputs for reading input[pin][samples]
-        inputs = [[0] * (2 * adcSamp) for _ in range(self.channelNum)] #2 cycles per function call
-
-        #while running == True:
-        #read the value of the pin and write to the input array 
-        for j in range(len(2*adcSamp))
-            for i in range(len(self.channelNum)):
-             inputs[i][j] = GPIO.input(self.pins[i])
-         
-        time.sleep(wait)
 
     def write_to_socket(self, emg, movement: Optional[int] = None):
         """Writes data to socket. On every sample, we pickle the data and send it to the socket.
