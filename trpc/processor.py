bf454638726ca0da0a339f3d2e3b20100273573c
jacob.yao <jacob@blackcape.io>
2024-01-28 17:54:52 -0500

    Add getter functions

--- trpc/processor.py
+++ trpc/processor.py
@@ -16,8 +16,16 @@ class Processor:
         self.classifier = self._set_up_classifier()
         self.classifier.run(block=False)
 
+    def get_data_handler(self):
+        """Returns the data handler"""
+        return self.odh
+    
+    def get_classifier(self):
+        """Returns the classifier"""
+        return self.classifier
+
     def _set_up_classifier(self, window_size: int = 40, window_increment: int = 20,
-                          feature_set: str = 'LS4', model: str = "SVM"):
+                          feature_set: str = 'LS4', model: str = "LDA"):
         """Creates the feature classifier for the processor.
 
         Args:
@@ -26,6 +34,7 @@ class Processor:
             feature_set:  The group of features to extract. See the get_feature_list() function for valid options.
             model: Model to use for the classifier
         """
+        # Step 1: Parse training data
         classes_values = ["0","1","2","3","4"]
         classes_regex = make_regex(left_bound = "_C_", right_bound=".csv", values = classes_values)
         reps_values = ["0", "1", "2", "3"]
@@ -41,19 +50,23 @@ class Processor:
         odh.get_data(folder_location='data/', filename_dic=dic, delimiter=',')
         train_windows, train_meta = odh.parse_windows(window_size, window_increment)
 
+        # Step 2: Extract features from training data
         fe = FeatureExtractor()
         feature_list = fe.get_feature_groups()[feature_set]
         training_features = fe.extract_features(feature_list, train_windows)
 
+        # Step 3: Create training data set
         data_set = {'training_features': training_features, 'training_labels': train_meta['classes']}
 
+        # Step 4: Train the classifier on the training data
         emg = EMGClassifier()
         emg.fit(model=model, feature_dictionary=data_set.copy())
+        # Having velocity control is useful for proportional control of the robot. See
+        # https://libemg.github.io/libemg/documentation/classification/classification.html#velocity-control-sup-4-sup
+        emg.add_velocity(train_windows, train_meta['classes'])
         emg.add_rejection(0.8)
 
-        return OnlineEMGClassifier(emg, window_size, window_increment, self.odh, fe.get_feature_groups()[feature_set],
-                                   std_out=True)
-
+        return OnlineEMGClassifier(emg, window_size, window_increment, self.odh, fe.get_feature_groups()[feature_set])
 
     def close(self) -> None:
         """Closes the processor"""
