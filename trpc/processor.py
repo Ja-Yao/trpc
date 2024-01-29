ab80a916555c19649ff68c50194360a2c4aedba5
jacob.yao <jacob@blackcape.io>
2024-01-28 19:27:21 -0500

    Refactor to define run function

--- trpc/processor.py
+++ trpc/processor.py
@@ -1,31 +1,32 @@
+from typing import List
+
 from libemg.data_handler import OnlineDataHandler, OfflineDataHandler
 from libemg.feature_extractor import FeatureExtractor
 from libemg.emg_classifier import EMGClassifier, OnlineEMGClassifier
 from libemg.utils import make_regex
 
 
-class Processor:
-    """Processor class for processing data from a device to the LibEMG pipeline"""
+class TRPCProcessor:
+    """Class for processing data from a device to the LibEMG pipeline
 
-    def __init__(self) -> None:
+        Args:
+            window_size: Size of the window for the classifier
+            window_increment: Increment of the window for the classifier
+            feature_set: The group of features to extract. This can be a string or a custom list of features. See the
+                         get_feature_groups() function for predefined groups.
+            model: Model to use for the classifier
+    """
+
+    def __init__(self, window_size: int = 40, window_increment: int = 20, feature_set: str | List[str] = "LS4",
+                 model: str = "LDA") -> None:
         # The handler listens to UDP port 12345 by default, with ip address 127.0.0.1.
         # No need to specify these parameters unless we need to change them.
         self.odh = OnlineDataHandler()
         self.odh.start_listening()
 
-        self.classifier = self._set_up_classifier()
-        self.classifier.run(block=False)
+        self.classifier = self._set_up_classifier(window_size, window_increment, feature_set, model)
 
-    def get_data_handler(self):
-        """Returns the data handler"""
-        return self.odh
-    
-    def get_classifier(self):
-        """Returns the classifier"""
-        return self.classifier
-
-    def _set_up_classifier(self, window_size: int = 40, window_increment: int = 20,
-                          feature_set: str = 'LS4', model: str = "LDA"):
+    def _set_up_classifier(self, window_size: int, window_increment: int, feature_set: str | List[str], model: str):
         """Creates the feature classifier for the processor.
 
         Args:
@@ -34,6 +35,7 @@ class Processor:
             feature_set:  The group of features to extract. See the get_feature_list() function for valid options.
             model: Model to use for the classifier
         """
+
         # Step 1: Parse training data
         classes_values = ["0","1","2","3","4"]
         classes_regex = make_regex(left_bound = "_C_", right_bound=".csv", values = classes_values)
@@ -52,7 +54,7 @@ class Processor:
 
         # Step 2: Extract features from training data
         fe = FeatureExtractor()
-        feature_list = fe.get_feature_groups()[feature_set]
+        feature_list = fe.get_feature_groups()[feature_set] if isinstance(feature_set, str) else feature_set
         training_features = fe.extract_features(feature_list, train_windows)
 
         # Step 3: Create training data set
@@ -66,7 +68,20 @@ class Processor:
         emg.add_velocity(train_windows, train_meta['classes'])
         emg.add_rejection(0.8)
 
-        return OnlineEMGClassifier(emg, window_size, window_increment, self.odh, fe.get_feature_groups()[feature_set])
+        return OnlineEMGClassifier(emg, window_size, window_increment, self.odh,
+                                   fe.get_feature_groups()[feature_set] if isinstance(feature_set, str)
+                                   else feature_set)
+
+    def get_data_handler(self):
+        """Returns the data handler"""
+        return self.odh
+
+    def get_classifier(self):
+        """Returns the classifier"""
+        return self.classifier
+
+    def run(self):
+        self.classifier.run(block=False)
 
     def close(self) -> None:
         """Closes the processor"""
