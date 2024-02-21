d6d823aab9f67bbba223fcd19bdd6e252b14c3fb
Thane <thanegallo@gmail.com>
2024-02-21 11:04:03 -0500

    doubled sample amount to attempt to get more than one signal

--- scripts/test_i2c_comms.py
+++ scripts/test_i2c_comms.py
@@ -1,4 +1,5 @@
 from trpc import Streamer
+import time
 
 #initialize streamer object
 my_streamer = Streamer()
@@ -6,10 +7,10 @@ my_streamer = Streamer()
 #setup i2c module 
 handle = my_streamer.i2c_setup()
 
-signal_per_sec = 128 # default value
 
+sample_num = 128 # default value
 
-for i in range(signal_per_sec):
+for i in range(sample_num*2):
     my_streamer.analog_input_selector(handle)
     print(my_streamer.pins)
     i+=1
