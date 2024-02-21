155081141eea5d735721703ab2e41dc49ef9d0b6
Thane <thanegallo@gmail.com>
2024-02-21 11:34:05 -0500

    adjusted name #1

--- scripts/test_i2c_comms.py
+++ scripts/test_i2c_comms.py
@@ -5,14 +5,15 @@ import time
 my_streamer = Streamer()
 
 #setup i2c module 
-handle = my_streamer.i2c_setup()
+handle = my_streamer.setup_i2c()
 
 
 sample_num = 128 # default value
 
+#to collect a large number of input sets
 for i in range(sample_num*2):
-    my_streamer.analog_input_selector(handle)
-    print(my_streamer.pins)
+    my_streamer.analog_input_selector(handle) # collects pin data on 4 inputs
+    print(my_streamer.pins) # prints out set of pins per sample
     i+=1
 
     
