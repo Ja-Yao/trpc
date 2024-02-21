13b08eec6f5bf74505467814f82dbd300a12567b
Thane <thanegallo@gmail.com>
2024-02-21 10:14:39 -0500

    changed looping var and fixed permanent loop #1

--- scripts/test_i2c_comms.py
+++ scripts/test_i2c_comms.py
@@ -6,10 +6,13 @@ my_streamer = streamer()
 #setup i2c module 
 handle = my_streamer.i2c_setup()
 
+signal_per_sec = 128 # default value
 
-for i in range(4):
+
+for i in range(signal_per_sec):
     my_streamer.analog_input_selector(handle)
     print(my_streamer.pins)
+    i+=1
 
     
     
