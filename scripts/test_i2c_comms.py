043914378a71f6bd3e7b92e6a05f7c27e9a7f2f3
Thane <thanegallo@gmail.com>
2024-02-21 11:42:09 -0500

    pydantic syntax edits #1

--- scripts/test_i2c_comms.py
+++ scripts/test_i2c_comms.py
@@ -1,20 +1,28 @@
 from trpc import Streamer
 import time
-
-#initialize streamer object
-my_streamer = Streamer()
-
-#setup i2c module 
-handle = my_streamer.setup_i2c()
+import subprocess
 
 
-sample_num = 128 # default value
+if __name__ == "__main__":
+    subprocess.run(["sudo", "pigpiod"])
+    
+    #initialize streamer object
+    my_streamer = Streamer()
 
-#to collect a large number of input sets
-for i in range(sample_num*2):
-    my_streamer.analog_input_selector(handle) # collects pin data on 4 inputs
-    print(my_streamer.pins) # prints out set of pins per sample
-    i+=1
+    #setup i2c module 
+    handle = my_streamer.setup_i2c()
+
+
+    sample_num = 128 # default value
+    #to collect a large number of input sets
+    for i in range(sample_num*2):
+        my_streamer.analog_input_selector(handle) # collects pin data on 4 inputs
+        print(my_streamer.pins) # prints out set of pins per sample
+        i+=1
+        
+    subprocess.run(["sudo", "killall", "pigpiod"])
+    
+        
 
     
     
