a178c43444e98b6225174be05487442344cdaaa8
Thane <thanegallo@gmail.com>
2024-02-21 10:09:11 -0500

    fixed imports and loop for reading signal

--- scripts/test_i2c_comms.py
+++ scripts/test_i2c_comms.py
@@ -1,1 +1,16 @@
-import Streamer
+from trpc import streamer
+
+#initialize streamer object
+my_streamer = streamer()
+
+#setup i2c module 
+handle = my_streamer.i2c_setup()
+
+
+for i in range(4):
+    my_streamer.analog_input_selector(handle)
+    print(my_streamer.pins)
+
+    
+    
+
