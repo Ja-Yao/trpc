4a72dd57043fc1d51b4f0012f628e92cb1f7493d
Thane <thanegallo@gmail.com>
2024-02-21 16:26:39 -0500

    added plotting for test #1

--- scripts/test_i2c_comms.py
+++ scripts/test_i2c_comms.py
@@ -1,6 +1,7 @@
 from trpc import Streamer
 import time
 import subprocess
+import matplotlib as plt
 
 
 if __name__ == "__main__":
@@ -11,16 +12,25 @@ if __name__ == "__main__":
 
     #setup i2c module 
     handle = my_streamer.setup_i2c()
-
+    plot_data = []
 
     sample_num = 128 # default value
     #to collect a large number of input sets
     for i in range(sample_num*2):
         my_streamer.analog_input_selector() # collects pin data on 4 inputs
-        print(f"{hex(my_streamer.pins[0])}  {hex(my_streamer.pins[1])} {hex(my_streamer.pins[2])} {hex(my_streamer.pins[3])}")
+        #print(f"{hex(my_streamer.pins[0])}  {hex(my_streamer.pins[1])} {hex(my_streamer.pins[2])} {hex(my_streamer.pins[3])}")
+        plot_data.append(my_streamer.pins[0])
+
+
+# Plot the data
+plt.plot(plot_data)
+plt.xlabel('Sample')
+plt.ylabel('Value')
+plt.title('2\'s Complement Data Plot')
+plt.grid(True)
+plt.show()
         
-    subprocess.run(["sudo", "killall", "pigpiod"])
-    
+        
         
 
     
