a202030dd63c6e2967bb1f5417eb52680590996e
Rpi <Thanegallo@gmail.com>
2024-02-22 11:42:27 -0500

    merge fix

--- scripts/test_i2c_comms.py
+++ scripts/test_i2c_comms.py
@@ -1,7 +1,7 @@
 from trpc import Streamer
 import time
 import subprocess
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
@@ -18,17 +18,26 @@ if __name__ == "__main__":
     #to collect a large number of input sets
     for i in range(sample_num*2):
         my_streamer.analog_input_selector() # collects pin data on 4 inputs
-        #print(f"{hex(my_streamer.pins[0])}  {hex(my_streamer.pins[1])} {hex(my_streamer.pins[2])} {hex(my_streamer.pins[3])}")
+        print(f"{hex(my_streamer.pins[0])}  {hex(my_streamer.pins[1])} {hex(my_streamer.pins[2])} {hex(my_streamer.pins[3])}")
         plot_data.append(my_streamer.pins[0])
 
+<<<<<<< Updated upstream
+=======
+
+    # Plot the data
+>>>>>>> Stashed changes
     plt.plot(plot_data)
     plt.xlabel('Sample')
     plt.ylabel('Value')
     plt.title('2\'s Complement Data Plot')
     plt.grid(True)
+<<<<<<< Updated upstream
     plt.show(block=False)
     
     exit(0)
+=======
+    plt.show()
+>>>>>>> Stashed changes
         
         
         
