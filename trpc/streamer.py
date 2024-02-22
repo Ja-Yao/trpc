7fb274d97ab2edde37fec52fb0ba8e88a5424882
Jacob Yao <jtyaos@gmail.com>
2024-02-22 13:21:52 -0500

    Setup ADS object

--- trpc/streamer.py
+++ trpc/streamer.py
@@ -1,6 +1,9 @@
-from abc import ABC, abstractmethod
-import socket
 import pickle
+import socket
+
+from abc import ABC, abstractmethod
+from ADS1x15 import ADS1115
+from time import sleep
 from typing import Optional
 from trpc.utils.logger import get_logger
 
@@ -37,6 +40,17 @@ class TRPCStreamer(Streamer):
         super().__init__()
         self._port = port
         self._ip_address = ip_address
+        self._adc = ADS1115(1)
+        self._adc.setMode(ADS1115.MODE_CONTINUOUS)
+
+    def read_emg(self):
+        self._adc.readADC(0)
+    
+        while True:
+            raw = self._adc.getValue()
+            logger.info("{0:.3f} V".format(self._adc.toVoltage(raw)))
+            sleep(1)
+            
 
     def write_to_socket(self, emg, movement: Optional[int] = None):
         data = {"emg": emg, "movement": movement}
