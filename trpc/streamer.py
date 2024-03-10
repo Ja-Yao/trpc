7317970ecbb3098b26f834f9e752c082c4a833e4
Jacob Yao <jtyaos@gmail.com>
2024-03-10 17:39:09 -0400

    Refactor base class implementations

--- trpc/streamer.py
+++ trpc/streamer.py
@@ -3,7 +3,6 @@ import socket
 
 from abc import ABC, abstractmethod
 from ADS1x15 import ADS1115
-# from datetime import datetime
 from typing import List
 from trpc.utils.logger import get_logger
 
@@ -12,8 +11,10 @@ logger = get_logger(__name__)
 
 class Streamer(ABC):
     """Abstract Streamer class for sending data from device to LibEMG pipeline"""
-    def __init__(self) -> None:
+    def __init__(self, port: int = 12345, ip_address: str = "") -> None:
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._port = port
+        self._ip_address = ip_address
 
     @property
     def socket(self):
@@ -24,27 +25,23 @@ class Streamer(ABC):
         """Reads EMG data from the device"""
         pass
 
-    @abstractmethod
     def write_to_socket(self, emg: List[float | int]):
         """Writes data to socket. On every sample, we pickle the data and send it to the socket.
         
         Args:
-            emg: EMG data to write
-            movement: Movement data
+            emg: EMG data to write. This should be a list of voltage values read from each channel
         """
-        pass
+        self._socket.sendto(pickle.dumps(emg), (self._ip_address, self._port))
 
-    @abstractmethod
     def close_socket(self):
         """Closes the socket"""
-        pass
+        self._socket.close()
+
 
 class TRPCStreamer(Streamer):
     """Streamer class for sending data from device to LibEMG pipeline"""
     def __init__(self, port: int = 12345, ip_address: str = ""):
-        super().__init__()
-        self._port = port
-        self._ip_address = ip_address
+        super().__init__(port, ip_address)
         self._adc = ADS1115(1)
         self._adc.setDataRate(7)
         self._adc.setGain(1)
@@ -52,8 +49,6 @@ class TRPCStreamer(Streamer):
 
     def read_emg(self):
         self._adc.requestADC(0)
-        # Uncomment the following commented lines to run the test_i2c.py script
-        # data = []
         try:
             while True:
                 if self._adc.isReady():
@@ -66,19 +61,10 @@ class TRPCStreamer(Streamer):
                     voltages.append(self._adc.toVoltage(self._adc.readADC(2)))
                     voltages.append(self._adc.toVoltage(self._adc.readADC(3)))
 
-                    self.write_to_socket(pickle.dumps(voltages), (self._ip_address, self._port))
+                    self.write_to_socket(voltages)
         except KeyboardInterrupt:
             logger.info("Interrupted by user. Closing socket and exiting...")
             self.close_socket()
             exit(0)
         except SystemExit:
             self.close_socket()
-
-        # return data
-            
-
-    def write_to_socket(self, emg: List[float | int]):
-        self._socket.sendto(pickle.dumps(emg), (self._ip_address, self._port))
-
-    def close_socket(self):
-        self._socket.close()
