27bf22f8924e7df0295b6eaa362677bc9c8c86eb
Jacob Yao <jtyaos@gmail.com>
2024-03-28 17:07:15 -0400

    Add classifier file path as cmd line argument

--- scripts/e2e.py
+++ scripts/e2e.py
@@ -1,4 +1,5 @@
 import subprocess
+from argparse import ArgumentParser
 
 from trpc import Controller, TRPCStreamer, TRPCProcessor
 from trpc.utils.logger import get_logger
@@ -6,9 +7,18 @@ from trpc.utils.logger import get_logger
 logger = get_logger(__name__)
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--classifier-file-path", type=str, help="Path to the classifier file")
+
+    args = parser.parse_args()
+    file_path = args.classifier_file_path
+
+    if not file_path:
+        raise ValueError("Please provide the path to the classifier file")
+
     subprocess.run(["sudo", "pigpiod"])
 
-    controller = Controller(streamer=TRPCStreamer(), classifier=TRPCProcessor())
+    controller = Controller(streamer=TRPCStreamer(), classifier=TRPCProcessor(classifier_path=file_path))
 
     try:
         controller.start()
